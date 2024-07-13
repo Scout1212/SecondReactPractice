1f5cfc7763d7b3530b4ca2f75aa458e14d9faae9
Scout1212 <RobertZamora.act@gmail.com>
2024-07-12 21:02:14 -0400

    Finished :)
    very quick -> gonna do more to learn more: then ill make my own project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './Components/MovieCard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [movies, setMovies] = useState([])
+    const [searchValue, setSearchValue] = useState('')
+
+    const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=9b772d81';
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    };
+
+    useEffect(() => {
+        searchMovies({searchValue})
+    }, [])
+
+    return(
+        <div className="app">
+            <h1>MovieLand</h1>
+            <div className="search">
+                <input placeholder="Search for movies" value= { searchValue } onChange={(e) => setSearchValue(e.target.value)}/>
+                <img src={SearchIcon} alt="search" onClick= {() => searchMovies(searchValue)}/>
+            </div>
+
+            {
+                movies?.length > 0 ? (
+                    <div className="container"> {movies.map((movie) => (<MovieCard movie = {movie} key={movie.imdbID}/>))} </div>):
+                    (<div className= "empty"> <h2>No Movies found</h2> </div>)
+            }
+        </div>
+    );
 }
 
 export default App;
